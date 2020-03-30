f0fbe3e906d2abb4f0f73e26b73f1d67e8e60d40
Heather Moreira <moreiraheather@gmail.com>
2020-03-30 03:34:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -14,19 +14,6 @@ def key_for_min_value(name_hash)
   return min_value 
 end
 
-# hash = {:blake => 500, :ashley => 2, :adam => 1}
-
-def key_for_min_value(name_hash)
-  min_value = nil
-  name_hash.collect do |key, value| 
-    if min_value == nil 
-      min_value = key 
-    elsif 1 < name_hash[2] 
-      min_value = adam  
-    end
-  end 
-  return min_value 
-end
 
 
 
